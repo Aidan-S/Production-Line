4b3f743ece0e8075000451b23176f23e9b69bdb2
scannella <scannella@bishopireton.org>
2017-11-29 12:04:12 -0500

    Initial Stuffs that I didnt commit earlier cause I did a bad

---
+++ src/ProductionLine.java
@@ -0,0 +1,27 @@
+import java.util.Queue;
+
+public class ProductionLine {
+	
+	Queue input;
+	Queue output;
+	Tower robot;
+	
+	public ProductionLine() {
+		
+		
+	}
+	
+	public addDisk()
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
